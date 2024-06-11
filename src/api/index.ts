6f1825b974c516927c191b8baab138ef77de7931
小新 <782446723@qq.com>
2024-06-11 17:50:03 +0800

    feat: 封装api常用方法

--- src/api/index.ts
+++ src/api/index.ts
@@ -6,6 +6,8 @@ import type {
     InternalAxiosRequestConfig,
 } from 'axios';
 import axios from 'axios';
+import { getToken } from '@/utils/auth';
+import type { ResultData } from '@/api/interface';
 
 const config = {
     // 默认地址请求地址，可在 .env.** 文件中修改
@@ -25,6 +27,10 @@ class RequestHttp {
 
         this.service.interceptors.request.use(
             (config: InternalAxiosRequestConfig) => {
+                const token = getToken();
+                if (token) {
+                    config.headers['token'] = token;
+                }
                 return config;
             },
             (error: AxiosError) => {
@@ -41,6 +47,49 @@ class RequestHttp {
                 return Promise.reject(error);
             },
         );
+    }
+
+    get<T>(
+        url: string,
+        params?: object,
+        _object = {},
+    ): Promise<ResultData<T>> {
+        return this.service.get(url, { params, ..._object });
+    }
+
+    post<T>(
+        url: string,
+        params?: object | string,
+        _object = {},
+    ): Promise<ResultData<T>> {
+        return this.service.post(url, params, _object);
+    }
+
+    put<T>(
+        url: string,
+        params?: object,
+        _object = {},
+    ): Promise<ResultData<T>> {
+        return this.service.put(url, params, _object);
+    }
+
+    delete<T>(
+        url: string,
+        params?: any,
+        _object = {},
+    ): Promise<ResultData<T>> {
+        return this.service.delete(url, { params, ..._object });
+    }
+
+    download(
+        url: string,
+        params?: object,
+        _object = {},
+    ): Promise<BlobPart> {
+        return this.service.post(url, params, {
+            ..._object,
+            responseType: 'blob',
+        });
     }
 }
 
