fd488287833de3249f4a84b9976da86c65c342eb
小新 <782446723@qq.com>
2024-06-19 17:38:43 +0800

    feat: 规范menu组件

--- src/layouts/components/XMenu/XMenu.vue
+++ src/layouts/components/XMenu/XMenu.vue
@@ -1,75 +1,32 @@
 <template>
     <el-menu
-        default-active="2"
+        :router="false"
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :unique-opened="accordion"
         class="border-0"
-        @select="handleSelect"
     >
-        <template
-            v-for="(item, index) in asideMenus"
-            :key="index"
-        >
-            <el-sub-menu
-                v-if="item.child && item.child.length > 0"
-                :index="item.name"
-            >
-                <template #title>
-                    <el-icon>
-                        <component :is="item.icon"></component>
-                    </el-icon>
-                    <span>{{ item.name }}</span>
-                </template>
-                <el-menu-item
-                    v-for="(item2, index2) in item.child"
-                    :key="index2"
-                    :index="item2.frontpath"
-                >
-                    <el-icon>
-                        <component :is="item2.icon"></component>
-                    </el-icon>
-                    <span>{{ item2.name }}</span>
-                </el-menu-item>
-            </el-sub-menu>
-
-            <el-menu-item v-else :index="item.frontpath">
-                <el-icon>
-                    <component :is="item.icon"></component>
-                </el-icon>
-                <span>{{ item.name }}</span>
-            </el-menu-item>
-        </template>
+        <XSubMenu :menu-list="subMenuList" />
     </el-menu>
 </template>
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
+import { useRoute } from 'vue-router';
+import XSubMenu from '@/layouts/components/XMenu/XSubMenu.vue';
+import { computed, ref } from 'vue';
+import { useGlobalStore } from '@/stores/modules/global';
 
-const router = useRouter();
-const asideMenus = [
-    {
-        name: '后台面板',
-        icon: 'help',
-        child: [
-            {
-                name: '主控台',
-                icon: 'home-filled',
-                frontpath: '/',
-            },
-        ],
-    },
-    {
-        name: '商城管理',
-        icon: 'shopping-bag',
-        child: [
-            {
-                name: '商品管理',
-                icon: 'shopping-cart-full',
-                frontpath: '/goods/list',
-            },
-        ],
-    },
-];
+const route = useRoute();
+const globalStore = useGlobalStore();
 
-const handleSelect = (e) => {
-    router.push(e);
-};
+const accordion = computed(() => globalStore.accordion);
+const isCollapse = computed(() => globalStore.isCollapse);
+const activeMenu = computed(
+    () =>
+        (route.meta.activeMenu
+            ? route.meta.activeMenu
+            : route.path) as string,
+);
+
+const subMenuList = ref<Menu.MenuOptions[]>([]);
 </script>
 <style scoped></style>
