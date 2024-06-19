582f02f1e773b19f2d34f7c6b10e72b1a85b79ed
小新 <782446723@qq.com>
2024-06-19 17:39:14 +0800

    chore: 导入全图标

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,15 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 import ElementPlus from 'element-plus';
+import * as Icons from '@element-plus/icons-vue';
 import 'element-plus/dist/index.css';
 
 import './styles/main.css';
 
 const app = createApp(App);
+Object.keys(Icons).forEach((key) => {
+    app.component(key, Icons[key as keyof typeof Icons]);
+});
 
 app.use(createPinia());
 app.use(ElementPlus);
